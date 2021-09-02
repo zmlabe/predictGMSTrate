5691dad5185027fcea50ca820d595ed19c2dd6bb
Zachary Labe <zmlabe@rams.colostate.edu>
2021-09-02 12:57:13 -0600

    OHC figures v1

---
+++ Scripts/plot_ObsPrediction-EDA_v1.py
@@ -0,0 +1,145 @@
+"""
+First exploratory data analysis for making plots of observations
+
+Author     : Zachary M. Labe
+Date       : 2 September 2021
+Version    : 1 (mostly for testing)
+"""
+
+### Import packages
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
+from netCDF4 import Dataset
+import scipy.stats as stats
+import palettable.cubehelix as cm
+import cmocean as cmocean
+import calc_Utilities as UT
+import calc_dataFunctions as df
+import calc_Stats as dSS
+from sklearn.metrics import accuracy_score
+
+### Plotting defaults 
+plt.rc('text',usetex=True)
+plt.rc('font',**{'family':'sans-serif','sans-serif':['Avant Garde']})
+
+### Parameters FOR THIS SCRIPT
+accurateR = False
+accurateH = False
+
+### Hyperparamters for files of the ANN model
+variq = 'T2M'
+fac = 0.8
+random_segment_seed = int(np.genfromtxt('/Users/zlabe/Documents/Research/GmstTrendPrediction/Data/SelectedSegmentSeed.txt',unpack=True))
+random_network_seed = 87750
+hidden = [15,15]
+n_epochs = 500
+batch_size = 128
+lr_here = 0.001
+ridgePenalty = 0.1
+actFun = 'relu'
+yearsall = np.arange(1990,2020+1,1)
+
+### Naming conventions for files
+directorymodel = '/Users/zlabe/Documents/Research/GmstTrendPrediction/SavedModels/'
+savename = 'ANN_'+variq+'_hiatus_' + actFun + '_L2_'+ str(ridgePenalty)+ '_LR_' + str(lr_here)+ '_Batch'+ str(batch_size)+ '_Iters' + str(n_epochs) + '_' + str(len(hidden)) + 'x' + str(hidden[0]) + '_SegSeed' + str(random_segment_seed) + '_NetSeed'+ str(random_network_seed)
+
+### Directories to save files
+directorydata = '/Users/zlabe/Documents/Research/GmstTrendPrediction/Data/'
+directoryfigure = '/Users/zlabe/Desktop/GmstTrendPrediction/ANN_v1/Obs/'
+
+###############################################################################
+###############################################################################
+###############################################################################
+### Read in data for observations
+predict_obs = np.genfromtxt(directorydata + 'obsLabels_' + savename + '.txt')
+actual_obs = np.genfromtxt(directorydata + 'obsActualLabels_' + savename + '.txt')
+confidence = np.genfromtxt(directorydata + 'obsConfid_' + savename + '.txt')
+
+###############################################################################
+###############################################################################
+###############################################################################
+def adjust_spines(ax, spines):
+    for loc, spine in ax.spines.items():
+        if loc in spines:
+            spine.set_position(('outward', 5))
+        else:
+            spine.set_color('none')  
+    if 'left' in spines:
+        ax.yaxis.set_ticks_position('left')
+    else:
+        ax.yaxis.set_ticks([])
+
+    if 'bottom' in spines:
+        ax.xaxis.set_ticks_position('bottom')
+    else:
+            ax.xaxis.set_ticks([]) 
+
+### Begin plot
+fig = plt.figure(figsize=(8,6))
+ax = plt.subplot(211)
+
+adjust_spines(ax, ['left', 'bottom'])
+ax.spines['top'].set_color('none')
+ax.spines['right'].set_color('none')
+ax.spines['left'].set_color('dimgrey')
+ax.spines['bottom'].set_color('dimgrey')
+ax.spines['left'].set_linewidth(2)
+ax.spines['bottom'].set_linewidth(2)
+ax.tick_params('both',length=4,width=2,which='major',color='dimgrey')
+ax.yaxis.grid(zorder=1,color='darkgrey',alpha=0.35,clip_on=False,linewidth=0.4)
+
+plt.plot(yearsall,confidence[:,0],linewidth=4,color='teal',alpha=1,zorder=3,clip_on=False,
+         label=r'\textbf{Climate Change}')
+plt.plot(yearsall,confidence[:,1],linewidth=2,color='maroon',alpha=1,zorder=3,clip_on=False,
+         linestyle='--',dashes=(1,0.3),label=r'\textbf{Hiatus}')
+
+plt.yticks(np.arange(0,2,0.1),map(str,np.round(np.arange(0,2,0.1),2)),size=6)
+plt.xticks(np.arange(1990,2030+1,10),map(str,np.arange(1990,2030+1,10)),size=6)
+plt.xlim([1990,2020])   
+plt.ylim([0,1])  
+
+leg = plt.legend(shadow=False,fontsize=11,loc='upper center',
+              bbox_to_anchor=(0.5,1.22),fancybox=True,ncol=4,frameon=False,
+              handlelength=5,handletextpad=1)
+for line,text in zip(leg.get_lines(), leg.get_texts()):
+    text.set_color(line.get_color())
+
+plt.text(1990,1.05,r'\textbf{[a]}',color='k',
+         fontsize=7,ha='center')  
+plt.ylabel(r'\textbf{Confidence}',color='k',fontsize=10)         
+
+###############################################################################
+ax = plt.subplot(212)
+
+adjust_spines(ax, ['left', 'bottom'])
+ax.spines['top'].set_color('none')
+ax.spines['right'].set_color('none')
+ax.spines['left'].set_color('dimgrey')
+ax.spines['bottom'].set_color('dimgrey')
+ax.spines['left'].set_linewidth(2)
+ax.spines['bottom'].set_linewidth(2)
+ax.tick_params('both',length=4,width=2,which='major',color='dimgrey')
+
+rects = plt.bar(yearsall,actual_obs)
+plt.plot(yearsall,predict_obs,linewidth=2,color='maroon',alpha=2,zorder=3,clip_on=False,
+         linestyle='--',dashes=(1,0.3))
+for i in range(len(rects)):
+    rects[i].set_color('maroon')
+    rects[i].set_edgecolor('w')
+    rects[i].set_alpha(0.4)
+
+plt.yticks(np.arange(0,2,1),map(str,np.round(np.arange(0,2,1),2)),size=6)
+plt.xticks(np.arange(1990,2030+1,10),map(str,np.arange(1990,2030+1,10)),size=6)
+plt.xlim([1990,2020])   
+plt.ylim([0,1])  
+
+plt.text(1990,0.9,r'\textbf{ACTUAL HIATUS}',fontsize=20,color='maroon',alpha=0.4)
+plt.text(1990,0.8,r'\textbf{{PREDICTED HIATUS}',fontsize=20,color='maroon',alpha=1)
+plt.text(1990,1.05,r'\textbf{[b]}',color='k',
+          fontsize=7,ha='center')  
+plt.ylabel(r'\textbf{Classification}',color='k',fontsize=10)      
+        
+plt.tight_layout()
+plt.subplots_adjust(hspace=0.4)
+plt.savefig(directoryfigure + 'Obs-EDA_ConfidencePredictions',dpi=300)
